760f81049cec206a0d2daa8fb55afeea4c519b2c
Willian Simas <64346201+willsimas@users.noreply.github.com>
2024-03-11 17:41:23 -0300

    Alterada a ordem de exibição e adicionado botões para exclusão e finalização da tarefa!

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const completedContainer = document.getElementById("completedContent")
     const formContent = document.getElementById("formContent")
     const addTask = document.getElementById("addTask")
-    
 
+    const btnDelete = document.getElementById("btn_delete")
+    const btnFinish = document.getElementById("btn_finish")
+    
     formContent.addEventListener("submit", function(event) {
         event.preventDefault()
-        completedContainer.innerHTML += (
-            `<li>${addTask.value}</li>`
+        ongoingContainer.innerHTML += (
+            `<li>${addTask.value} <button id="btn_delete">EXCLUIR</button><button id="btn_finish">FINALIZAR</button></li>`
         )
     })
+
+    if(btnDelete) {
+        
+    }
 })
